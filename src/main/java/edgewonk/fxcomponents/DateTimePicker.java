152e08bbe87c0c8f7f1be406d096013a83b9a310
Sergei Malafeev <sergeymalafeev@gmail.com>
2016-06-29 14:02:47 +0200

    add time to date picker

--- src/main/java/edgewonk/fxcomponents/DateTimePicker.java
+++ src/main/java/edgewonk/fxcomponents/DateTimePicker.java
@@ -1,16 +1,30 @@
 package edgewonk.fxcomponents;
 
+import com.sun.javafx.scene.control.skin.DatePickerContent;
+import com.sun.javafx.scene.control.skin.DatePickerSkin;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Skin;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-
-import java.time.LocalDateTime;
+import javafx.util.StringConverter;
 
 /**
  * This is a simple datetimepicker component. It was developed for the trade entry wizard in the edgewonk trading journal. It should do the following things:
@@ -27,27 +41,174 @@ import java.time.LocalDateTime;
 public class DateTimePicker extends Pane {
 
   private final ObjectProperty<LocalDateTime> dateTimeProperty = new SimpleObjectProperty<>();
-  private final BooleanProperty allowNull = new SimpleBooleanProperty(false);
-  private final BooleanProperty allowTime = new SimpleBooleanProperty(false);
-
-  @FXML private Button btnShowCalendar;
+  private final BooleanProperty allowNull = new SimpleBooleanProperty(true);
+  private final BooleanProperty allowTime = new SimpleBooleanProperty(true);
+  private final DatePicker datePicker;
+  private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+  private final TextField textHours = new TextField();
+  private final TextField textMinutes = new TextField();
 
   public DateTimePicker() {
-    // load fxml
-    getChildren().setAll(loadView());
+    allowTime.addListener((observable, oldValue, newValue) -> {
+      textHours.setEditable(newValue);
+      textMinutes.setEditable(newValue);
 
-    // bind properties, etc.
+      if(!newValue) {
+        textHours.setText("00");
+        textMinutes.setText("00");
+      }
+    });
+
+    textHours.setPrefColumnCount(2);
+    textHours.textProperty().addListener((ov, oldValue, newValue) -> {
+      if (!newValue.chars().allMatch(Character::isDigit)) {
+        textHours.setText(oldValue);
+        return;
+      }
+      if (textHours.getText().length() > 2) {
+        String s = textHours.getText().substring(0, 2);
+        textHours.setText(s);
+      }
+    });
+
+    textMinutes.setPrefColumnCount(2);
+
+    datePicker = new DatePicker() {
+      @Override
+      protected Skin<?> createDefaultSkin() {
+        DatePickerSkin datePickerSkin = new DatePickerSkin(this);
+        DatePickerContent datePickerContent = (DatePickerContent) datePickerSkin.getPopupContent();
+
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setSpacing(7.);
+
+        hBox.getChildren().addAll(textHours, new Label(":"), textMinutes);
+        datePickerContent.getChildren().add(0, hBox);
+
+        return datePickerSkin;
+      }
+    };
+
+    datePicker.setConverter(new StringConverter<LocalDate>() {
+      @Override
+      public String toString(LocalDate object) {
+        LocalDateTime value = getDateTime();
+        if (value == null) {
+          return "";
+        }
+
+        if (!allowTime.get()) {
+          value = value.withHour(0).withMinute(0).withSecond(0);
+        }
+
+        return value.format(formatter);
+      }
+
+      @Override
+      public LocalDate fromString(String value) {
+        if (value == null || (allowNull.get() && value.trim().isEmpty())) {
+          dateTimeProperty.set(null);
+          return null;
+        }
+
+        LocalDateTime localDateTime;
+        try {
+           localDateTime = LocalDateTime.parse(value, formatter);
+        } catch (DateTimeParseException e) {
+          localDateTime = dateTimeProperty.get();
+          if(localDateTime == null) {
+            return null;
+          }
+        }
+
+        if(allowTime.get()) {
+          textHours.setText(String.valueOf(localDateTime.getHour()));
+          textMinutes.setText(String.valueOf(localDateTime.getMinute()));
+        }
+
+        dateTimeProperty.set(localDateTime);
+        return dateTimeProperty.get().toLocalDate();
+      }
+    });
+
+    datePicker.setOnShowing(event -> {
+      if(dateTimeProperty.get() == null) {
+        if(allowTime.get()) {
+          LocalTime now = LocalTime.now();
+          textHours.setText(String.valueOf(now.getHour()));
+          textMinutes.setText(String.valueOf(now.getMinute()));
+        }
+      }
+      datePicker.getEditor().fireEvent(new KeyEvent(datePicker.getEditor(), datePicker.getEditor(), KeyEvent.KEY_PRESSED, null, null, KeyCode.ENTER, false, false, false, false));
+    });
+
+    datePicker.valueProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue == null) {
+        if (allowNull.get()) {
+          dateTimeProperty.set(null);
+        }
+      } else {
+        if (dateTimeProperty.get() == null) {
+          LocalTime localTime = LocalTime.now();
+          if (!textHours.getText().isEmpty()) {
+            localTime = localTime.withHour(Integer.parseInt(textHours.getText()));
+          }
+
+          if (!textMinutes.getText().isEmpty()) {
+            localTime = localTime.withMinute(Integer.parseInt(textMinutes.getText()));
+          }
+          dateTimeProperty.set(LocalDateTime.of(newValue, localTime));
+        } else {
+          LocalTime time = dateTimeProperty.get().toLocalTime();
+          int hours;
+          int minutes;
+          if (textHours.getText().isEmpty()) {
+            hours = time.getHour();
+          } else {
+            hours = Integer.parseInt(textHours.getText());
+          }
+
+          if (textMinutes.getText().isEmpty()) {
+            minutes = time.getMinute();
+          } else {
+            minutes = Integer.parseInt(textMinutes.getText());
+          }
+          time = LocalTime.of(hours, minutes);
+
+          dateTimeProperty.set(LocalDateTime.of(newValue, time));
+        }
+      }
+    });
+
+    // Synchronize changes to dateTimePropertyProperty back to the underlying date value
+    dateTimeProperty.addListener((observable, oldValue, newValue) -> {
+      if (oldValue != null && newValue == null && !allowNull.get()) {
+        dateTimeProperty.setValue(oldValue);
+      }
+
+      datePicker.setValue(newValue == null ? null : newValue.toLocalDate());
+    });
+
+    // Persist changes onblur
+    datePicker.getEditor().focusedProperty().addListener((observable, oldValue, newValue) -> {
+      if (!newValue) {
+        datePicker.getEditor().fireEvent(new KeyEvent(datePicker.getEditor(), datePicker.getEditor(), KeyEvent.KEY_PRESSED, null, null, KeyCode.ENTER, false, false, false, false));
+      }
+    });
+
+    getChildren().setAll(loadView());
   }
 
-  public LocalDateTime getDateTimeProperty() {
+  public LocalDateTime getDateTime() {
     return dateTimeProperty.get();
   }
 
-  public ObjectProperty<LocalDateTime> dateTimePropertyProperty() {
+  public ObjectProperty<LocalDateTime> dateTimeProperty() {
     return dateTimeProperty;
   }
 
-  public void setDateTimeProperty(final LocalDateTime dateTimeProperty) {
+  public void setDateTime(final LocalDateTime dateTimeProperty) {
     this.dateTimeProperty.set(dateTimeProperty);
   }
 
@@ -76,8 +237,8 @@ public class DateTimePicker extends Pane {
   }
 
   private Node loadView() {
-    // TODO not implemented
-    return new StackPane();
+    StackPane stackPane = new StackPane();
+    stackPane.getChildren().add(datePicker);
+    return stackPane;
   }
-
 }
---
+++ src/test/java/edgewonk/fxcomponents/DateTimePickerTest.java
@@ -0,0 +1,56 @@
+package edgewonk.fxcomponents;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import javafx.embed.swing.JFXPanel;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+
+public class DateTimePickerTest {
+  JFXPanel jfxPanel;
+
+  @Before
+  public void init() {
+    jfxPanel = new JFXPanel();
+  }
+
+  @Test
+  public void testNotAllowNull() {
+    DateTimePicker dateTimePicker = new DateTimePicker();
+    dateTimePicker.setAllowNull(false);
+    LocalDateTime localDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 15));
+    dateTimePicker.dateTimeProperty().setValue(localDateTime);
+    dateTimePicker.dateTimeProperty().setValue(null);
+    assertEquals(localDateTime, dateTimePicker.dateTimeProperty().getValue());
+    assertEquals(localDateTime, dateTimePicker.getDateTime());
+  }
+
+  @Test
+  public void testAllowNull() {
+    DateTimePicker dateTimePicker = new DateTimePicker();
+    dateTimePicker.setAllowNull(true);
+    LocalDateTime localDateTime = LocalDateTime.now();
+    dateTimePicker.dateTimeProperty().setValue(localDateTime);
+    dateTimePicker.dateTimeProperty().setValue(null);
+    assertNull(dateTimePicker.dateTimeProperty().getValue());
+    assertNull(dateTimePicker.getDateTime());
+  }
+
+  @Test
+  public void testNotAllowTime() {
+    DateTimePicker dateTimePicker = new DateTimePicker();
+    dateTimePicker.setAllowTime(false);
+    LocalDateTime localDateTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 12));
+    dateTimePicker.dateTimeProperty().setValue(localDateTime);
+    assertEquals(0, dateTimePicker.getDateTime().getHour());
+    assertEquals(0, dateTimePicker.getDateTime().getMinute());
+  }
+
+}
